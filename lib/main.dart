6af2fa21a3973dda016993f4858739a0fbae8815
Yomal <freezyomal2@gmail.com>
2023-06-23 22:28:54 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:movie_finder/theme/custom_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Finder',
+      darkTheme: CustomTheme.darkTheme(context),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
---
+++ lib/theme/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  static const Color almostBlack = Color(0xff040B13);
+  static const Color red100 = Color(0xFFFFF5F5);
+  static const Color red200 = Color(0xFFFFE5E5);
+  static const Color red300 = Color(0xFFFFC4C4);
+  static const Color red400 = Color(0xFFFF9B9B);
+  static const Color red500 = Color(0xFFFF7B7B);
+  static const Color red600 = Color(0xFFFA5252);
+  static const Color red700 = Color(0xFFF03E3E);
+  static const Color red800 = Color(0xFFE03131);
+  static const Color red900 = Color(0xFFC92A2A);
+}
