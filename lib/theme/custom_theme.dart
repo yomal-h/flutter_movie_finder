6fe79e30628edbf46f6b965c4461707d9e8a31d5
Yomal <freezyomal2@gmail.com>
2023-06-23 22:38:09 +0200

    Bug fix

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -7,20 +7,22 @@ class CustomTheme {
     return ThemeData(
       brightness: Brightness.dark,
       colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: MaterialColor(
-        Palette.red500.value,
-        const {
-          100: Palette.red100,
-          200: Palette.red200,
-          300: Palette.red300,
-          400: Palette.red400,
-          500: Palette.red500,
-          600: Palette.red600,
-          700: Palette.red700,
-          800: Palette.red800,
-          900: Palette.red900,
-        },
-      )).copyWith(secondary: Palette.red500),
+        brightness: Brightness.dark, // Set brightness to Brightness.dark
+        primarySwatch: MaterialColor(
+          Palette.red500.value,
+          const {
+            100: Palette.red100,
+            200: Palette.red200,
+            300: Palette.red300,
+            400: Palette.red400,
+            500: Palette.red500,
+            600: Palette.red600,
+            700: Palette.red700,
+            800: Palette.red800,
+            900: Palette.red900,
+          },
+        ),
+      ).copyWith(secondary: Palette.red500),
       scaffoldBackgroundColor: Palette.almostBlack,
       appBarTheme: const AppBarTheme(
         elevation: 0,
