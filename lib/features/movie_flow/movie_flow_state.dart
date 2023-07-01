8433571727329c27fb8e47d6c5f3c30a7e7535dc
Yomal <freezyomal2@gmail.com>
2023-07-01 23:39:34 +0200

    modified:   lib/features/movie_flow/movie_flow_state.dart

--- lib/features/movie_flow/movie_flow_state.dart
+++ lib/features/movie_flow/movie_flow_state.dart
@@ -1,50 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_finder/features/movie_flow/genre/genre.dart';
 import 'package:movie_finder/features/movie_flow/result/movie.dart';
 
-const movieMock = Movie(
-  title: 'Batman',
-  overview:
-      'lorem ipm hggt iopjj yui gfr ffr ht  h th tththh h hthhtht yyyjhjj  yjyjyj  yjyj  yht hth tt   htgrrgrgrrg rgr gr g rg rgrg',
-  voteAverage: 4.8,
-  genres: [Genre(name: 'Action'), Genre(name: 'Fantasy')],
-  releaseDate: '24-05-2018',
-  backdropPath: '',
-  posterPath: '',
-);
+// const movieMock = Movie(
+//   title: 'Batman',
+//   overview:
+//       'lorem ipm hggt iopjj yui gfr ffr ht  h th tththh h hthhtht yyyjhjj  yjyjyj  yjyj  yht hth tt   htgrrgrgrrg rgr gr g rg rgrg',
+//   voteAverage: 4.8,
+//   genres: [Genre(name: 'Action'), Genre(name: 'Fantasy')],
+//   releaseDate: '24-05-2018',
+//   backdropPath: '',
+//   posterPath: '',
+// );
 
-const genresMock = [
-  Genre(name: 'Action'),
-  Genre(name: 'Comedy'),
-  Genre(name: 'Drama'),
-  Genre(name: 'Thriller'),
-  Genre(name: 'Romance'),
-  Genre(name: 'Adventure'),
-  Genre(name: 'Sci-Fi'),
-];
+// const genresMock = [
+//   Genre(name: 'Action'),
+//   Genre(name: 'Comedy'),
+//   Genre(name: 'Drama'),
+//   Genre(name: 'Thriller'),
+//   Genre(name: 'Romance'),
+//   Genre(name: 'Adventure'),
+//   Genre(name: 'Sci-Fi'),
+// ];
 
 @immutable
 class MovieFlowState {
   final PageController pageController;
   final int rating;
   final int yearsBack;
-  final List<Genre> genres;
-  final Movie movie;
+  final AsyncValue<List<Genre>> genres;
+  final AsyncValue <Movie> movie;
 
   const MovieFlowState({
     required this.pageController,
     this.rating = 5,
     this.yearsBack = 10,
-    this.genres = genresMock,
-    this.movie = movieMock,
+    required this.genres,
+    required this.movie,
   });
 
   MovieFlowState copyWith({
     PageController? pageController,
     int? rating,
     int? yearsBack,
-    List<Genre>? genres,
-    Movie? movie,
+    AsyncValue<List<Genre>>? genres,
+    AsyncValue <Movie>? movie,
   }) {
     return MovieFlowState(
         pageController: pageController ?? this.pageController,
